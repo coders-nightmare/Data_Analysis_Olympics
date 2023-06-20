680f6c493101728d77b9bd1a91fc706afd9c9db0
Ritik <ritik09052000@gmail.com>
2023-06-20 11:30:39 +0530

    completed country wise analysis

--- helper.py
+++ helper.py
@@ -85,3 +85,20 @@ def yearwise_medal_tally(df,country):
     final_df=new_df.groupby('Year').count()['Medal'].reset_index()
 
     return final_df
+
+def country_event_heatmap(df,country):
+    temp_df=df.dropna(subset=['Medal'])
+    temp_df.drop_duplicates(['Team','NOC','Games','Year','City','Sport','Event','Medal'],inplace=True)
+
+    new_df=temp_df[temp_df['region']==country]
+    pt=new_df.pivot_table(index='Sport',columns='Year',values='Medal',aggfunc='count').fillna(0)
+    return pt
+
+def most_successful_countrywise(df,country):
+    temp_df=df.dropna(subset=['Medal'])
+    
+    temp_df=temp_df[temp_df['region']==country]
+    x= temp_df['Name'].value_counts().reset_index().head(10).merge(df,on='Name',how='left')[['Name','count','Sport']].drop_duplicates('Name')
+    x.rename(columns={'count':'Medals'},inplace=True)
+    return x
+
